17885dd75ffefc7ddf70aa4835c57d03c60cef8c
Franklin <Franklinau2@gmail.com>
2024-04-27 12:01:59 -0700

    Add img_name field to game model

--- models/game.js
+++ models/game.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const GameSchema = new Schema({
   name: { type: String, required: true },
   characters: [{ type: Schema.Types.ObjectId, ref: "Character" }],
+  img_name: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Game", GameSchema);
