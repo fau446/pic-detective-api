d25e45860373c2dff7ec9e9d976b4da8b38a0b52
Franklin <Franklinau2@gmail.com>
2024-04-30 10:13:05 -0700

    Add Leaderboard model

---
+++ models/leaderboard.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+
+const Schema = mongoose.Schema;
+
+const LeaderboardSchema = new Schema({
+  name: { type: String, required: true },
+  game: { type: Schema.Types.ObjectId, ref: "Game" },
+  time: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
+});
+
+LeaderboardSchema.virtual("date_formatted").get(function () {
+  return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATETIME_MED);
+});
+
+module.exports = mongoose.model("Leaderboard", LeaderboardSchema);
